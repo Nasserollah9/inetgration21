260de80f7e457f42f930f985a4b811fcb2c1dbb5
Nasserollah9 <nasro.elyamami@gmail.com>
2025-05-14 16:55:31 +0100

    Ajoutez des fichiers projet.

---
+++ inetgration21/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inetgration21.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
